56db0cfa47f01adf0ffb154413abed6a7b9ad53d
sarbagyaphu-hub <sarbagya.phu@gmail.com>
2026-07-25 23:00:15 +0545

    Update simplecodes.py

--- simplecodes.py
+++ simplecodes.py
@@ -49,4 +49,44 @@ var_num13 = 50
 var_num14 = 11
 print ("If we substract 11 from 50 then we will get: ", var_num13-var_num14)
 
+#Eleventh Code
+var_num15 = 16
+var_num16 = var_num15*var_num15
+print ("The Squared value of 16 is: ", var_num16)
 
+#Twelveth Code
+var_num17 = 25
+print ("The squared value of 25 would be: ", var_num17*var_num17)
+
+#Thirteenth Code
+var_num18 = 6
+var_num19 = var_num18*var_num18*var_num18
+print ("The Cube of 18 is: ", var_num19)
+
+#Fourteenth Code
+var_num20 = 9
+print ("The cube of number 19 is: ", var_num20*var_num20*var_num20)
+
+#Fifteenth Code
+var_num21 = 81
+var_num22 = 9
+var_num23 = var_num21/var_num22
+print ("The Squareroot of 81 is: ", var_num23)
+
+#Sixteenth Code
+var_num24 = 25
+var_num25 = 5
+print ("The squareroot of 25 is :", var_num24/var_num25 )
+
+#Seventeenth Code
+var_num26 = 64
+var_num27 = 4
+var_num28 = var_num27*var_num27
+var_num29 = var_num26/var_num28
+print ("The cube root of number 64 is: ", var_num29)
+
+#Eighteenth Code
+var_num30 = 8
+var_num31 = 2
+var_num32 = var_num31*var_num31
+print ("The cuberoot of 8 is: ", var_num30/var_num32)
