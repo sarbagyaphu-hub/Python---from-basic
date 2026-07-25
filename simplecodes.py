5d8efdd735d57149ed02ab069bfb90d6c89d0627
sarbagyaphu-hub <sarbagya.phu@gmail.com>
2026-07-25 22:42:42 +0545

    Update simplecodes.py

--- simplecodes.py
+++ simplecodes.py
@@ -1,1 +1,52 @@
+#first program
+print ("Hello World")
+
+# second Program
+var = "hello world"
+print (var)
+
+#third Program
+var_1 = 10
+var_2 = 15
+var_3 = var_1+var_2
+print ("The Sum Of Two Given Numbers Would Be: ", var_3)
+
+#fourth program
+var_a = 25
+var_b = 30
+print ("The Sum Of Given Numbers Would Be ",var_a+var_b)
+
+#fifth program
+var_num = 2
+var_num1 = 5
+var_num2 = var_num * var_num1
+print ("If we multiply these two given numbers we will get: ", var_num2)
+
+#sixth program
+var_num3 = 2
+var_num4 = 5
+print ("If we multiply these two given numbers we will get: ", var_num3*var_num4)
+
+#seventh Program
+var_num5 = 60
+var_num6 = 30
+var_num7 = var_num5 / var_num6
+print ("If we Divide these two given numbers we will get: ", var_num7)
+
+#eighth Program
+var_num8 = 80
+var_num9 = 60
+print ("If we Divide these two given numbers we will get: ", var_num8/var_num9)
+
+#Nine Program
+var_num10 = 88
+var_num11 = 3
+var_num12 = var_num10 - var_num11
+print ("If we substract 3 from 88 then we will get: ", var_num12)
+
+#Tenth Code
+var_num13 = 50
+var_num14 = 11
+print ("If we substract 11 from 50 then we will get: ", var_num13-var_num14)
+
 
