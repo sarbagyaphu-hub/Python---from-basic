f30ee47d400d5ceff06f3bea06e35f9002252a5b
sarbagyaphu-hub <sarbagya.phu@gmail.com>
2026-07-26 11:35:34 +0545

    Update python_codesimplex.py

--- python_codesimplex.py
+++ python_codesimplex.py
@@ -1,1 +1,46 @@
+#User Input Code
+#First Code
+var_1 = input ("Plaease enter any text: ")
+print ("You have entered: ", var_1)
 
+#Second Code
+print (input("Please enter a text:"))
+
+#Third Code
+var_2 = int(input("please enter a number:"))
+var_3 = int(input("please enter num to sum it with:"))
+var_4 = var_2+var_3
+print ("The sum of given two numbers are: ", var_4)
+
+#Fourth Code
+var_5 = int(input("please enter a number:"))
+var_6 = int(input("please enter num to sum it with:"))
+print ("The sum of given two numbers are: ", var_5+var_6)
+
+#Fifth Code
+var_7 = int(input("please enter a number:"))
+var_8 = int(input("please enter a number to substract given number above:"))
+var_9 = var_7-var_8
+print ("If we subtract ", var_8, " from ", var_7, " we will get ", var_9)
+
+#Sixth Code
+var_10 = int(input("please enter a number:"))
+var_11 = int(input("please enter a number to multiply with:"))
+var_12 = var_10*var_11
+print ("If we multiply given number then we will get: ", var_12)
+
+#Seventh Code
+var_13 = int(input("please enter a number:"))
+var_14 = int(input("please enter a number to multiply with:"))
+print ("If we multiply given number then we will get: ", var_13*var_14)
+
+#Eighth Code
+var_15 = int(input("please enter a number:"))
+var_16 = int(input("please enter a number to divide with:"))
+var_17 = var_15/var_16
+print ("If we multiply given number then we will get: ", var_17)
+
+#Nineth Code
+var_18 = int(input("please enter a number:"))
+var_19 = int(input("please enter a number to multiply with:"))
+print ("If we multiply given number then we will get: ", var_18/var_19)
