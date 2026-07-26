fc1977a9a8578dec49c310edff99c627b7f196b8
sarbagyaphu-hub <sarbagya.phu@gmail.com>
2026-07-26 18:04:08 +0545

    Create pythonwhileloop.py

---
+++ pythonwhileloop.py
@@ -0,0 +1,26 @@
+#While loop
+#First Code
+var_1 = "Nepal"
+count =1
+print ("printing Nepal 2 times")
+while count<=2:
+    print (var_1)
+    count += 1
+
+#Second Code
+var_2 = 2
+count = 1
+print ("Multiplication Table of 2:")
+while count<=10:
+    var_3 = var_2*count
+    print (count, "*", var_2, " = ", var_3)
+    count += 1
+
+#Third Code
+var_2 = 2
+count = 10
+print ("Multiplication Table of 2:")
+while count>=1:
+    var_3 = var_2*count
+    print (count, "*", var_2, " = ", var_3)
+    count -= 1
