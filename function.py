f6a41c40a1fb3f64db4d030d43ecadf909988694
sarbagyaphu-hub <sarbagya.phu@gmail.com>
2026-07-26 18:25:07 +0545

    Create function.py

---
+++ function.py
@@ -0,0 +1,13 @@
+#Function
+#First Code
+print ("Code using function")
+def display():
+    print ("Hello World")
+display()
+
+#Second Code
+def personal_details():
+    name = input("Please Enter Your Name: \n")
+    age = int(input("Please enter your age: \n"))
+    print ("welcome Mr.", name, " You are actually ", age, " years old but look much young than that")
+personal_details()
